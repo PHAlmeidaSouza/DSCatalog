e1aeceecb0904df6556209dcfd909a13accb6dde
Pedro Henrique <pedro.henrique@itpower.co>
2023-11-08 14:20:36 -0300

    Find by id metod iadded

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,13 @@ public class CategoryService {
 		 * List<CategoryDTO> listDto = new ArrayList<>(); for (Category cat : list) {
 		 * listDto.add(new CategoryDTO(cat)); }
 		 */
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
