dba35418a2aade71b6ea8089204e0f3ea18b51be
Pedro Henrique <ph8584774@gmail.com>
2023-11-21 22:02:13 -0300

    Test: delete by null id

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Optional;
 
@@ -19,6 +20,14 @@ public class ProductRepositoryTests {
         productRepository.deleteById(1L);
         Optional<Product> result = productRepository.findById(1L);
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+
+        Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+            productRepository.deleteById(1000L);
+        });
     }
 
 }
