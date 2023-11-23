4040fb374d2a37b1c3d54bf801c285cb905f5477
Pedro Henrique <pedro.henrique@itpower.co>
2023-11-23 09:17:21 -0300

    Atts

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,19 +16,39 @@ public class ProductRepositoryTests {
 
     @Autowired
     private ProductRepository productRepository;
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    public void setUp() throws Exception{
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = productRepository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts +1, product.getId());
+    }
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        productRepository.deleteById(1L);
-        Optional<Product> result = productRepository.findById(1L);
+        productRepository.deleteById(existingId);
+        Optional<Product> result = productRepository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
     }
 
     @Test
     public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
-
         Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-            productRepository.deleteById(1000L);
+            productRepository.deleteById(nonExistingId);
         });
     }
 
