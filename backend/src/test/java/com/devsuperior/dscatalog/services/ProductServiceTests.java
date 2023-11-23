e94f4f08c2a7475145abc8fcd775fcc48d3b07a6
Pedro Henrique <ph8584774@gmail.com>
2023-11-23 20:45:45 -0300

    First Mockito Configuration

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dscatalog.services;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private Long existingId;
+    private Long nonExistingId;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.doNothing().when(productRepository).deleteById(existingId);
+        Mockito.doThrow(EmptyResultDataAccessException.class).when(productRepository).deleteById(nonExistingId);
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(() -> {
+            productService.delete(existingId);
+        });
+        Mockito.verify(productRepository, Mockito.times(1)).deleteById(existingId);
+    }
+}
