347c655face89e3a6c3accea0643cd1d6b09187e
Pedro Henrique <ph8584774@gmail.com>
2023-11-27 22:33:03 -0300

    ProductResourceTests created

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -1,5 +1,24 @@
 package com.devsuperior.dscatalog.resources;
 
+import com.devsuperior.dscatalog.services.ProductService;
+import org.assertj.core.internal.Classes;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(ProductResource.class)
 public class ProductResourceTests {
-    
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService service;
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
 }
