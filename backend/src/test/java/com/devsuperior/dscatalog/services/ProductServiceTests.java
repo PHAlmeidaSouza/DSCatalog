85df28103b34233039a03de96de4db7179f5abf9
Pedro Henrique <ph8584774@gmail.com>
2024-04-16 23:02:26 -0300

    Fix - ProductServiceTests

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -22,6 +24,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +36,9 @@ public class ProductServiceTests {
     private ProductService service;
 
     @Mock
+    private CategoryRepository categoryRepository;
+
+    @Mock
     private ProductRepository repository;
 
     private long existingId;
@@ -40,6 +46,9 @@ public class ProductServiceTests {
     private long dependentId;
     private PageImpl<Product> page;
     private Product product;
+    private Category category;
+
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp() throws Exception {
@@ -48,15 +57,51 @@ public class ProductServiceTests {
         dependentId = 3L;
         product = Factory.createdProduct();
         page = new PageImpl<>(List.of(product));
+        category = Factory.createCategory();
+        productDTO = Factory.createdProductDTO();
 
         Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+
         Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+
         Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
         Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        Mockito.when(repository.getOne(existingId)).thenReturn(product);
+        Mockito.when(repository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
+
+        Mockito.when(categoryRepository.getOne(existingId)).thenReturn(category);
+        Mockito.when(categoryRepository.getOne(nonExistingId)).thenThrow(EntityNotFoundException.class);
 
         doNothing().when(repository).deleteById(existingId);
         doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
         doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.update(nonExistingId, productDTO);
+        });
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() {
+        ProductDTO result = service.update(existingId, productDTO);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void findByIdShouldThrowResourceNotFoundExceptionWhenIdNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.findById(nonExistingId);
+        });
+    }
+
+    @Test
+    public void findByIdShouldReturnProductDTOWhenIdExists() {
+        ProductDTO result = service.findById(existingId);
+        Assertions.assertNotNull(result);
     }
 
     @Test
--- backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -9,12 +9,16 @@ import java.time.Instant;
 public class Factory {
     public static Product createdProduct() {
         Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
-        product.getCategories().add(new Category(2L, "Electronics"));
+        product.getCategories().add(createCategory());
         return product;
     }
 
     public static ProductDTO createdProductDTO() {
         Product product = createdProduct();
         return new ProductDTO(product, product.getCategories());
+    }
+
+    public static Category createCategory() {
+        return new Category(1L, "Electronics");
     }
 }
